17f477d96c03fe9d271291a7ad8ea747ddac64ad
Jon Savage <jon@studioaceofspades.com>
2018-10-22 19:02:54 +0000

    updated wordpress and plugins

--- public_html/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ public_html/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInitbc42a58bc348cc5d6aeb1496135741cb::getLoader();
+return ComposerAutoloaderInite61ebc052b87168a8bf6325f81d190f9::getLoader();
