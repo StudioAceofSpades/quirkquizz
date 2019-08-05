a81a87e2a1fb88ec6f50bc8db6ffef8903338aef
schiem <schiem01@gmail.com>
2019-08-05 13:32:09 -0400

    Icon documentation

--- Gulpfile.js
+++ Gulpfile.js
@@ -106,6 +106,12 @@ gulp.task('watch', function() {
         });
 });
 
+/** Add a font awesome icon.
+This adds both the SASS, allowing the class `fa fa-icon`
+to be added, as well as regenerating the font files.
+Usage: 
+gulp icon-add --icon {{icon}} --to {{default|brands}}
+**/
 gulp.task('icon-add', function(a) {
     if(arg.icon) {
         //Load our configurations
@@ -127,11 +133,16 @@ gulp.task('icon-add', function(a) {
             var currentIcons = JSON.parse(iconsRaw);
         }
 
+        //What font weight / group should the icon be added to
         var to = arg.to || conf.default; 
 
         //Figure out the names of the fonts being generated
         var types = conf.aliases[to];
+
+        //Map the icon names to codepoints - the codepoints will be the same across different font weights
         var codepoints = {};
+
+        //Start the code point at the beginning of the Unicode custom section
         var currentCodePoint = 0xf101;
 
         for(var i = 0; i < types.length; i++) {
@@ -143,22 +154,25 @@ gulp.task('icon-add', function(a) {
                 currentIcons[type] = [];
             }
             
-            //Make sure the icon doesn't already exist
+            //Make sure the icon doesn't already exist for that font weight
             if(currentIcons[type].indexOf(icon) !== -1) {
                 log("Icon already exists for " + type); 
             } else {
                 currentIcons[type].push(icon);
             }
 
-            //Get the file names
+            //Gather the SVG file names for all of the icons in our font
             var startPoint = currentCodePoint;
             var files = [];
             for(var j = 0; j < currentIcons[type].length; j++) {
                 var activeIcon = currentIcons[type][j];
+
+                //Generate the file name
                 var filePath = iconInput + thisConf.directory + "/" + activeIcon + ".svg"
                 if(fs.existsSync(filePath)) {
                     files.push(filePath);
-
+                    
+                    //Make sure that we haven't already assigned this icon a code point
                     if(!codepoints[activeIcon]) {
                         codepoints[activeIcon] = currentCodePoint;
                         currentCodePoint += 1;
@@ -193,7 +207,7 @@ gulp.task('icon-add', function(a) {
             }
         });
 
-        //Re create the JSON file
+        //Re create the JSON file for existing icons in the font
         fs.writeFile(activeIcons, JSON.stringify(currentIcons), function(err) {
             if(err) {
                 log(err);
