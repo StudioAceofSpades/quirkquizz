75e4c0a11bf338b60c1bd9c50d11d949106bf434
saosdev <jon@studioaceofspades.com>
2020-01-08 17:34:51 +0000

    creating the base CMS and UI for the admin area for quizzes

--- public_html/wp-content/themes/premise/functions.php
+++ public_html/wp-content/themes/premise/functions.php
@@ -144,4 +144,61 @@ function saos_output_link($link, $classes) {
 	}
 }
 
+function create_quiz_cpt() {
+
+	$labels = array(
+		'name' => _x( 'Quizzes', 'Post Type General Name', 'textdomain' ),
+		'singular_name' => _x( 'Quiz', 'Post Type Singular Name', 'textdomain' ),
+		'menu_name' => _x( 'Quizzes', 'Admin Menu text', 'textdomain' ),
+		'name_admin_bar' => _x( 'Quiz', 'Add New on Toolbar', 'textdomain' ),
+		'archives' => __( 'Quiz Archives', 'textdomain' ),
+		'attributes' => __( 'Quiz Attributes', 'textdomain' ),
+		'parent_item_colon' => __( 'Parent Quiz:', 'textdomain' ),
+		'all_items' => __( 'All Quizzes', 'textdomain' ),
+		'add_new_item' => __( 'Add New Quiz', 'textdomain' ),
+		'add_new' => __( 'Add New', 'textdomain' ),
+		'new_item' => __( 'New Quiz', 'textdomain' ),
+		'edit_item' => __( 'Edit Quiz', 'textdomain' ),
+		'update_item' => __( 'Update Quiz', 'textdomain' ),
+		'view_item' => __( 'View Quiz', 'textdomain' ),
+		'view_items' => __( 'View Quizzes', 'textdomain' ),
+		'search_items' => __( 'Search Quiz', 'textdomain' ),
+		'not_found' => __( 'Not found', 'textdomain' ),
+		'not_found_in_trash' => __( 'Not found in Trash', 'textdomain' ),
+		'featured_image' => __( 'Featured Image', 'textdomain' ),
+		'set_featured_image' => __( 'Set featured image', 'textdomain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'textdomain' ),
+		'use_featured_image' => __( 'Use as featured image', 'textdomain' ),
+		'insert_into_item' => __( 'Insert into Quiz', 'textdomain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Quiz', 'textdomain' ),
+		'items_list' => __( 'Quizzes list', 'textdomain' ),
+		'items_list_navigation' => __( 'Quizzes list navigation', 'textdomain' ),
+		'filter_items_list' => __( 'Filter Quizzes list', 'textdomain' ),
+	);
+	$args = array(
+		'label' => __( 'Quiz', 'textdomain' ),
+		'description' => __( '', 'textdomain' ),
+		'labels' => $labels,
+		'menu_icon' => 'dashicons-welcome-learn-more',
+		'supports' => array('title', 'excerpt', 'revisions', 'author', 'page-attributes', 'post-formats', 'custom-fields'),
+		'taxonomies' => array(),
+		'public' => true,
+		'show_ui' => true,
+		'show_in_menu' => true,
+		'menu_position' => 5,
+		'show_in_admin_bar' => true,
+		'show_in_nav_menus' => true,
+		'can_export' => true,
+		'has_archive' => true,
+		'hierarchical' => false,
+		'exclude_from_search' => false,
+		'show_in_rest' => true,
+		'publicly_queryable' => true,
+		'capability_type' => 'post',
+	);
+	register_post_type( 'quiz', $args );
+
+}
+add_action( 'init', 'create_quiz_cpt', 0 );
+
 ?>
