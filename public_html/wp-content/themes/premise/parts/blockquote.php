f8980995a497a3ee101c0337e87a7c9deacd5032
Jon Savage <jon@studioaceofspades.com>
2020-01-15 15:46:50 +0000

    removed unused parts files

--- public_html/wp-content/themes/premise/parts/blockquote.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-    <div class="col-sm-10 col-sm-offset-1">
-        <div class="quote">
-            <p><?php the_sub_field('quote'); ?></p>
-            <span><?php the_sub_field('attribution'); ?></span>
-        </div>
-    </div>
-</div>
