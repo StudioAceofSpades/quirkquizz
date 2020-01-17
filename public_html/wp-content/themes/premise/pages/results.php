aacb5e821376746c91c2eee9b190e4342801aa9b
Jon Savage <jon@studioaceofspades.com>
2020-01-17 20:03:47 +0000

    results are functional

--- public_html/wp-content/themes/premise/pages/results.php
+++ public_html/wp-content/themes/premise/pages/results.php
@@ -2,35 +2,81 @@
 if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
 if (CFCT_DEBUG) { cfct_banner(__FILE__); }
 
+$quiz_id    = $_GET['quiz_id'];
+$result_id  = $_GET['result'];
+$error      = false;
+
+if(!$quiz_id || !$result_id) {
+    $error = true;
+}
+
 get_header(); ?>
 
 <div id="results">
 	<div class="content reduce-padding">
         <div class="container">
             <div class="row">
+            <?php if($error): ?>
+                <div class="col-lg-8">
+                    <div class="card question">
+                        <h3 class="result-header">Uhoh. Looks like something went wrong.</h3>
+                        <h1>You've somehow reached this page by accident</h1>
+                        <div class="quiz-description">
+                            <p><br></p>
+                        </div>
+                        <div class="buttons center">
+                            <a href="<?php bloginfo('url'); ?>" class="button large ib purple">Head back to the homepage</a>
+                        </div>
+                    </div>
+                </div>
+            <?php else: ?>
                 <div class="col-lg-8">
                     <div class="card question">
 
                         <h3 class="result-header">Quiz Results</h3>
                         
-                        <h1>This is the result you got after taking the quiz</h1>
-                        
-                        <img class="featured-image" src="<?php bloginfo('template_directory'); ?>/img/demo/7.jpg" alt="">
-                        
-                        <div class="quiz-meta">
+                        <?php
+                        if(have_rows('results', $quiz_id)):
+                            $counter = 0;
+                            while(have_rows('results', $quiz_id)) : 
+                                the_row();
+                                $counter++;
+                                if($counter == $result_id) :
+                                ?>
+                                
+                                <?php if($title = get_sub_field('result_title')): ?>
+                                <h1><?php echo $title; ?></h1>
+                                <?php endif; ?>
+                                
+                                <?php if($image = get_sub_field('result_image')): ?>
+                                <img 
+                                    class="featured-image" 
+                                    src="<?php echo $image['sizes']['quiz_image']; ?>" 
+                                    alt="<?php echo $image['alt']; ?>">
+                                <?php endif; ?>
+                                
+                                <?php if($description = get_sub_field('result_description')): ?>
+                                <div class="quiz-meta">
+                                    <div class="quiz-description">
+                                        <?php echo $description; ?>
+                                    </div>
+                                </div>
+                                <?php endif; ?>
 
-                            <div class="quiz-description">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.</p>
-                            </div>
+                                <div class="buttons center">
+                                    <a href="#start-quiz" class="button large ib purple">Share Results</a>
+                                    <a href="#start-quiz" class="button large ib purple">Random Quiz</a>
+                                </div>
 
-                        </div>
+                                <?php
+                                endif;
+                            endwhile;
+                        endif;
+                        ?>
 
-                        <div class="buttons center">
-                            <a href="#start-quiz" class="button large ib purple">Share Results</a>
-                            <a href="#start-quiz" class="button large ib purple">Random Quiz</a>
-                        </div>
                     </div>
                 </div>
+            <?php endif; ?>
                 <div class="col-lg-4">
                     <aside class="sidebar">
                         <h2 class="section-header">More Quizzes</h2>
