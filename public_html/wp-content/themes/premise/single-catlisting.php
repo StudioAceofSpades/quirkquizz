2954aa427382b8a42ec3585215897815d85cfcb0
antphan98 <antphan98@gmail.com>
2020-10-13 20:49:01 +0000

    test commit

---
+++ public_html/wp-content/themes/premise/single-catlisting.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Template Name: Cat Listing Template
+*/
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+cfct_page('cat-listing');
+
+?>
