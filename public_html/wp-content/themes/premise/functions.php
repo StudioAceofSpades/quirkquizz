006abfe0527b1fc06e371e7e9224c069b99576c4
Jon Savage <jon@studioaceofspades.com>
2020-10-21 18:33:08 +0000

    Squashed commit of the following:
    commit 92a8d6488b13c99234c18a92b5844b783deb14a6
    Author: saosdev <jon@studioaceofspades.com>
    Date:   Wed Oct 21 18:30:39 2020 +0000
    
        formatting and code qa
    
    commit 53240ee61d5c0e8059f13348d2725869f7f43eb3
    Merge: 35b17548 d686e308
    Author: antphan98 <antphan98@gmail.com>
    Date:   Wed Oct 21 18:01:44 2020 +0000
    
        added desc for break
    
    commit 35b17548b25398d2d12c4c030fbf553610e278ef
    Author: antphan98 <antphan98@gmail.com>
    Date:   Wed Oct 21 17:48:28 2020 +0000
    
        Added break to fix pagination get results issue
    
    commit d686e308682b0f32e88eee82c1bff2c03042ee52
    Author: SrishtiAvalakki <spram001@odu.edu>
    Date:   Wed Oct 21 17:48:20 2020 +0000
    
        Fixed the Get results
    
    commit 443efd9c6d2f962df29b53b6584e61709cab9475
    Author: antphan98 <antphan98@gmail.com>
    Date:   Wed Oct 21 16:25:15 2020 +0000
    
        results button working
    
    commit 1efe3d0ef0f1cdc1efca8455d28449b536ec2aa2
    Author: Jon Savage <jon@studioaceofspades.com>
    Date:   Wed Oct 21 14:59:42 2020 +0000
    
        Removed demo ACF field groups. Added override for pagination to single quiz. Var dumping a broken variable that is preventing pagination from working correctly
    
    commit ae78a44af2ee66bb332da2f321be2ad09cf549a1
    Merge: 54d27fd7 11fe9761
    Author: antphan98 <antphan98@gmail.com>
    Date:   Tue Oct 20 20:54:38 2020 +0000
    
        fixing conflict merge
    
    commit 54d27fd7f467c42536570df4f787b32faced28e4
    Author: antphan98 <antphan98@gmail.com>
    Date:   Tue Oct 20 20:50:34 2020 +0000
    
        cleaned up code and added descriptions in code
    
    commit 11fe97612777d2ef2c055198f67fbb422cb92d73
    Merge: b17b3b15 c53d7b0e
    Author: SrishtiAvalakki <spram001@odu.edu>
    Date:   Tue Oct 20 16:17:30 2020 +0000
    
        Fixing the deleted files
    
    commit b17b3b1549fd9455706da9cbc373800349cc87bb
    Author: SrishtiAvalakki <spram001@odu.edu>
    Date:   Tue Oct 20 16:04:13 2020 +0000
    
        Deleting unwanted files from previous push
    
    commit c53d7b0eece4a46597d66f4a1d4a0d2e351127ff
    Merge: 19d76166 174b8cc4
    Author: antphan98 <antphan98@gmail.com>
    Date:   Tue Oct 20 15:59:24 2020 +0000
    
        fixing merge conflict
    
    commit 19d76166efe92ba12855935e9613bda07f0e9c5b
    Author: antphan98 <antphan98@gmail.com>
    Date:   Tue Oct 20 15:13:26 2020 +0000
    
        Test
    
    commit 174b8cc43883b6a9f4634594ed9da438fff1a74e
    Author: SrishtiAvalakki <spram001@odu.edu>
    Date:   Tue Oct 20 14:48:38 2020 +0000
    
        The number of Question Limitation per page using the Maximum Question  field
    
    commit 5652204d07b9cbb021fb4ca586b936781edbd4dc
    Author: SrishtiAvalakki <spram001@odu.edu>
    Date:   Tue Oct 13 20:50:33 2020 +0000
    
        test commit
    
    commit 2954aa427382b8a42ec3585215897815d85cfcb0
    Author: antphan98 <antphan98@gmail.com>
    Date:   Tue Oct 13 20:49:01 2020 +0000
    
        test commit

--- public_html/wp-content/themes/premise/functions.php
+++ public_html/wp-content/themes/premise/functions.php
@@ -214,6 +214,46 @@ function create_quiz_cpt() {
 }
 add_action( 'init', 'create_quiz_cpt', 0 );
 
+function cptui_register_my_cpts_cats() {
+
+	/**
+	 * Post Type: Cats.
+	 */
+
+	$labels = [
+		"name" => __( "Cats", "custom-post-type-ui" ),
+		"singular_name" => __( "Cat", "custom-post-type-ui" ),
+	];
+
+	$args = [
+		"label" => __( "Cats", "custom-post-type-ui" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"has_archive" => false,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"rewrite" => [ "slug" => "cats", "with_front" => true ],
+		"query_var" => true,
+		"supports" => [ "title", "editor", "thumbnail" ],
+	];
+
+	register_post_type( "cats", $args );
+}
+
+add_action( 'init', 'cptui_register_my_cpts_cats' );
+
+
 function track_views() {
 	wp_enqueue_script(
 		'track-views',
---
+++ public_html/wp-content/themes/premise/single-cats.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+Template Name: cats Template
+*/
+
+if (__FILE__ == $_SERVER['SCRIPT_FILENAME']) { die(); }
+if (CFCT_DEBUG) { cfct_banner(__FILE__); }
+
+cfct_page('cats');
+
+?>
+
