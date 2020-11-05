0e4c1d415e166b10c2ea3a64e5e6bca915649458
Jon Savage <jon@studioaceofspades.com>
2020-11-05 20:10:25 +0000

    code cleanup, qa, and formatting

--- public_html/wp-content/themes/premise/js/ga-events.js
+++ public_html/wp-content/themes/premise/js/ga-events.js
@@ -4,33 +4,19 @@
     });	
 
     function bindEvents(e){
-        
-        //setting up a goal in the Google Analytics that takes the click on the next page button on the Quiz page
+        // setting up a goal in the Google Analytics that takes the click on the next page button on the Quiz page
         $('#advance-button').click(function(e){
             ga('send', 'event', 'Quiz', 'Click', 'Next Page');
         });
 
-        //if the user lands on the landing page/starting page of the Quiz
+        // if the user lands on the landing page/starting page of the Quiz
         if ($('#start-quiz').length) {
             ga('send', 'event', 'Quiz', 'Loaded', 'Quiz Start');
         };
 
-        //setting up a goal if the user click on the Get Results button.
+        // setting up a goal if the user click on the Get Results button.
         $('#funnel-button').click(function(e){
             ga('send', 'event', 'Quiz', 'Click', 'Get Results');
         });
-
-
-
-
-
-
-        }
-
-
-
-
-    
-    
-
+    }
 })( jQuery );
