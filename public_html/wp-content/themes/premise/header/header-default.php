de2645c80c212c14b5122b9690bcf542e61451e6
Jon Savage <jon@studioaceofspades.com>
2021-02-18 21:44:35 +0000

    HOTFIX: Added Snap Pixel to the header

--- public_html/wp-content/themes/premise/header/header-default.php
+++ public_html/wp-content/themes/premise/header/header-default.php
@@ -26,6 +26,24 @@ E-Mail: jon@studioaceofspade.com
     var hasoffers_domain = "https://masstraffic.go2cloud.org";
     </script>
     <script type="text/javascript" src="https://media.go2app.org/assets/js/dl.js"></script>
+
+	<!-- Snap Pixel Code -->
+	<script type='text/javascript'>
+	(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+	{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+	a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+	r.src=n;var u=t.getElementsByTagName(s)[0];
+	u.parentNode.insertBefore(r,u);})(window,document,
+	'https://sc-static.net/scevent.min.js');
+
+	snaptr('init', '72f62000-d19e-462a-8f88-038b587f6cc6', {
+	'user_email': '__INSERT_USER_EMAIL__'
+	});
+
+	snaptr('track', 'PAGE_VIEW');
+
+	</script>
+	<!-- End Snap Pixel Code -->
     
 </head>
 
